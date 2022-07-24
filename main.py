dccd4412a8c3b6155a341d0ef43fbfec273bb536
Yash <iyashsrivastav@gmail.com>
2022-07-24 23:24:38 +0530

    intialized exit()

--- main.py
+++ main.py
@@ -3,33 +3,32 @@ import pyttsx3
 import speech_recognition as sr
 import datetime
 
-engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init('dummy')
 voices = engine.getProperty('voices')
 
 engine.setProperty('voice', voices[0].id)
 
 
+
 def takeInput():
     """Take User input through microphone & returns string output """
-    count = 0
-    while count == 0:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            r.pause_threshold = 1
-            audio = r.listen(source)
-        try:
-            print("Recognizing...")
-            query = r.recognize_google(audio, language="en-in")
-            print(f"User Said: {query}\n")
-            count += 1
-        except Exception:
-            print("Say That Again Please...")
-            return 'None'
-        return query
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User Said: {query}\n")
+        exit()
+    except Exception as e:
+        print("Say That Again Please...")
+        return 'None'
+
+    return query
 
 
 while True:
     say = takeInput().lower()
-    if 'exit' in say:
-        sys.exit()
+
