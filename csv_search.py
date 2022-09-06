177836893f71790e82b76587869e72975c9bc0d1
Yash Srivastava <iyashsrivastav@gmail.com>
2022-09-06 19:31:47 +0530

    searching through dask is done.

--- csv_search.py
+++ csv_search.py
@@ -5,8 +5,9 @@ import numpy as np
 import time
 import dask.dataframe as dd
 import csv
-
 # key_to_search = input('Enter the searching element: ')
+
+#optimised one
 key_to_search = 'CORN'
 start = time.time()
 dtypes = {
@@ -19,20 +20,12 @@ dtypes = {
     "FID": "uint8"
 
 }
-
-# data = dd.read_csv('traffic_crashes.csv', delimiter=",")
-data = pd.read_csv('traffic_crashes.csv', delimiter=",")
+# data = pd.read_csv('traffic_crashes.csv', delimiter=",")
+data = dd.read_csv('traffic_crashes.csv', delimiter=",")
 # data = csv.reader(open('traffic_crashes.csv', "r"), delimiter=",")
-# pd_df = data.compute()
-# ddf_select= dd[]
-
-# print(data)
-# print(data.partitions)
-# print("Train size:", pd_df.shape)
-for row in data:
-    # print(row)
-    if key_to_search == row[1]:
-        print(row)
 
 end = time.time()
 print(end - start, 'sec')
+
+print(data[data['LOCAT'] == 'CORN'].head())
+
